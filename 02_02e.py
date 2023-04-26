4c12aff50635ff93646bd0b57851f0b46f764556
Kedar Damkondwar <39631662+KedarD1@users.noreply.github.com>
2023-04-26 08:37:25 +0000

    commit

--- 02_02e.py
+++ 02_02e.py
@@ -1,15 +1,19 @@
 from collections import deque
 
+
 def check_palindrome(word):
     d = deque(word)
     while len(d) > 1:
         if d.pop() != d.popleft():
             return False
     return True
+
+
 def main():
-    word = 'tacocat'
+    word = 'kedek'
     print(check_palindrome(word))
     return
+
 
 if __name__ == "__main__":
     main()
